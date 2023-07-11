455915f99f16151aa654a3a6255ce7dd046787be
falseAzure <falseazure@proton.me>
2023-07-11 16:25:38 +0200

    Create topic_extractor.py

---
+++ infineac/topic_extractor.py
@@ -0,0 +1,28 @@
+"""
+This file contains helper functions for the infineac package.
+"""
+
+from bertopic import BERTopic
+from bertopic.representation import KeyBERTInspired
+
+
+def bert_basic(docs):
+    topic_model = BERTopic(
+        verbose=True, low_memory=True, nr_topics=10, calculate_probabilities=False
+    )
+    topics, probs = topic_model.fit_transform(docs)
+    return topics, probs
+
+
+def bert_inspired(docs):
+    # Fine-tune your topic representations
+    representation_model = KeyBERTInspired()
+    topic_model = BERTopic(
+        representation_model=representation_model,
+        verbose=True,
+        low_memory=True,
+        nr_topics=10,
+        calculate_probabilities=False,
+    )
+    topics, probs = topic_model.fit_transform(docs)
+    return topics, probs
