8e9e9ee2a7d68d26ff8b01a4e546b6c1ef3e86d7
falseAzure <falseazure@proton.me>
2023-07-11 16:28:43 +0200

    Create extract_topics.py

---
+++ scripts/extract_topics.py
@@ -0,0 +1,63 @@
+import argparse
+from pathlib import Path
+
+import infineac.file_loader as file_loader
+import infineac.process_event as process_event
+import infineac.topic_extractor as topic_extractor
+
+PATH = "data/transcripts/"
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        "This script generates topics from with BERTopic"
+        + "from a given list of files containing earnings calls transcripts."
+    )
+
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        default=PATH,
+        help="Path to directory of earnings calls transcripts",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--type",
+        type=str,
+        default="basic",
+        choices=["basic", "inspired"],
+        help="Type of BERTopic model to use: basic or inspired",
+    )
+
+    return parser.parse_args()
+
+
+if "__main__" == __name__:
+    args = get_args()
+    path = args.path
+
+    files = list(Path(path).rglob("*.xml"))
+    print(f"Found {len(files)} files\n")
+    print(f"Loading files from {files[0]} to {files[len(files) - 1]}")
+
+    events = file_loader.load_files_from_xml(files[0:500])
+
+    print("\nFilter events")
+    events_russia = process_event.filter_events(
+        events, year=2022, keywords={"russia": 1, "ukraine": 1}
+    )
+
+    print(f"Parsing {len(events_russia)} events")
+    docs = process_event.extract_docs_from_events(events_russia, ["russia", "ukraine"])
+
+    print(f"Extracting topics from {len(docs)} documents")
+    if args.type == "inspired":
+        topics, probs = topic_extractor.bert_inspired(docs)
+    elif args.type == "basic":
+        topics, probs = topic_extractor.bert_basic(docs)
+
+    print("Writing topics to output.txt\n")
+    with open("output.txt", "w") as f:
+        f.write(topics + "\n\n\n" + probs)
