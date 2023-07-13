c3c34af199ee2c1e8c69ed4bb2b9c49fb34fe11a
falseAzure <falseazure@proton.me>
2023-07-13 09:54:24 +0200

    Update process_event.py
    Refactor and add descriptions

--- infineac/process_event.py
+++ infineac/process_event.py
@@ -34,7 +34,21 @@ from tqdm import tqdm
 import infineac.process_text as process_text
 
 
-def extract_parts_from_presentation(presentation: list, keywords: list) -> str:
+def extract_paragraphs_from_presentation(presentation: list, keywords: list) -> str:
+    """
+    Method to extract important paragraphs from
+    the presentation part of an event.
+    Importance of a paragraph is determined by the presence of a keyword.
+    If a keyword is present in a paragraph the whole paragraph
+    as well as the subsequent one is extracted.
+
+    Args:
+        presentation (list): List of dicts containing the presentation part.
+        keywords (list): List of keywords to determine importance.
+
+    Returns:
+        str: The extracted parts as a concatenated string.
+    """
     whole_text = ""
     if presentation is None:
         return whole_text
@@ -55,18 +69,27 @@ def extract_parts_from_presentation(presentation: list, keywords: list) -> str:
     return whole_text
 
 
-def extract_parts_from_qa(qa: list, keywords: list) -> str:
+def extract_paragraphs_from_qa(qa: list, keywords: list) -> str:
+    """
+    Method to extract important paragraphs from the Q&A part of an event.
+    Importance of a paragraph is determined by the presence of a keyword.
+    If a keyword is present in a paragraph the whole paragraph
+    as well as the subsequent one is extracted.
+
+    Args:
+        qa (list): List of dicts containing the Q&A part.
+        keywords (list): List of keywords to determine importance.
+
+    Returns:
+        str: The extracted parts as a concatenated string.
+    """
     whole_text = ""
     if qa is None:
         return whole_text
     previous_question_keyword = False
     previous_paragraph_keyword = False
     for part in qa:
-        # operator, editor, moderator
-        # if part["position"] in ["operator", "editor", "moderator"]:
-        #     continue
-
-        # conference and others (unidentified, unknown etc.)
+        # conference participants and others (operator, unidentified, unknown etc.)
         if part["position"] != "cooperation":
             previous_question_keyword = False
             if any(keyword in part["text"].lower() for keyword in keywords):
@@ -90,25 +113,58 @@ def extract_parts_from_qa(qa: list, keywords: list) -> str:
     return whole_text
 
 
-def extract_text_from_event(event: dict, keywords: list) -> str:
+def extract_paragraphs_from_event(event: dict, keywords: list) -> str:
+    """
+    Method to extract important paragraphs from an event.
+
+    Args:
+        event (dict): Dict containing the event.
+        keywords (list): List of keywords to determine importance.
+
+    Returns:
+        str: The extracted parts as a concatenated string.
+    """
     doc = (
-        extract_parts_from_presentation(event["presentation"], keywords)
+        extract_paragraphs_from_presentation(event["presentation"], keywords)
         + "\n"
-        + extract_parts_from_qa(event["qa"], keywords)
+        + extract_paragraphs_from_qa(event["qa"], keywords)
     )
     return doc
 
 
-def extract_texts_from_events(events: list, keywords: list) -> list:
-    print("Extracting texts from events")
+def extract_paragraphs_from_events(events: list, keywords: list) -> list:
+    """
+    Method to extract important paragraphs from a list of events.
+
+    Args:
+        events (list): List of dicts containing the events.
+        keywords (list): List of keywords to determine importance.
+
+    Returns:
+        list: The extracted paragraphs as a list of concatenated strings.
+    """
+    print("Extracting paragraphs from events")
     docs = [
-        extract_text_from_event(event, keywords)
+        extract_paragraphs_from_event(event, keywords)
         for event in tqdm(events, desc="Events", total=len(events))
     ]
     return docs
 
 
 def check_keywords_in_event(event: dict, keywords: dict = {}) -> bool:
+    """
+    Method to check if keywords are present in the presentation or
+    Q&A part of an event.
+
+    Args:
+        event (dict): Dict containing the event.
+        keywords (dict, optional): Dict of keywords, where the key is the
+        keyword and the value is the number of appearances of that keyword.
+        Defaults to {}.
+
+    Returns:
+        bool: True if keywords are present, False otherwise.
+    """
     return process_text.check_keywords_in_string(
         string=str(event["qa_collapsed"] + event["presentation_collapsed"]),
         keywords=keywords,
@@ -116,7 +172,25 @@ def check_keywords_in_event(event: dict, keywords: dict = {}) -> bool:
 
 
 def filter_events(events: list, year: int = 2022, keywords: dict = {}) -> list:
+    """
+    Method to filter events based on the year and keywords.
+    All events before the given year are filtered out.
+    All events that do not contain the keywords in
+    the presentation and Q&Q part are filtered out.
+
+    Args:
+        events (list): List of dicts containing the events.
+        year (int, optional): Year. Defaults to 2022.
+        keywords (dict, optional): Dict of keywords, where the key is the
+        keyword and the value is the number of appearances of that keyword.
+        Defaults to {}.
+
+    Returns:
+        list: List of filtered events.
+    """
     print("Filtering events")
+    filtered_events = [e for e in events if e["type"] == "PushEvent"]
+    print("Filtered to {} events".format(len(filtered_events)))
     events_filtered = []
     for event in tqdm(events, desc="Events", total=len(events)):
         if not (
