1482a34d6d4f84fca911000bc8a487d0ba2a86aa
falseAzure <falseazure@proton.me>
2023-08-14 19:55:30 +0200

    Update pipeline.py

--- infineac/pipeline.py
+++ infineac/pipeline.py
@@ -3,12 +3,14 @@ from pathlib import Path
 import infineac.file_loader as file_loader
 import infineac.process_event as process_event
 import infineac.topic_extractor as topic_extractor
+from infineac.process_text import FILTER_WORDS
 
 
 def pipeline(
     path,
     year=2022,
     keywords={"russia": 1, "ukraine": 1},
+    filter_words=FILTER_WORDS,
     context_window_sentence=0,
     subsequent_paragraphs=0,
     extract_answers=True,
@@ -26,9 +28,16 @@ def pipeline(
     docs = process_event.extract_parts_from_events(
         events_filtered,
         keywords=keywords,
+        filter_words=filter_words,
         context_window_sentence=context_window_sentence,
         subsequent_paragraphs=subsequent_paragraphs,
         extract_answers=extract_answers,
     )
 
     topics, probs = topic_extractor.bert_inspired(docs)
+
+    return topics, probs
+
+
+def parameter_tuning():
+    pass
