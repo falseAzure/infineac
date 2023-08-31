b28cea40e5a84c2c0a1c406c2647b674715979ad
falseAzure <falseazure@proton.me>
2023-08-31 12:21:38 +0200

    Update pipeline.py
    adjusted to functions

--- infineac/pipeline.py
+++ infineac/pipeline.py
@@ -10,31 +10,39 @@ def pipeline(
     path,
     year=2022,
     keywords={"russia": 1, "ukraine": 1},
-    filter_words=MODIFIER_WORDS,
+    modifier_words=MODIFIER_WORDS,
     context_window_sentence=0,
     subsequent_paragraphs=0,
+    join_adjacent_sentences=True,
     extract_answers=True,
+    nlp_model=None,
+    remove_additional_words=True,
 ):
     files = list(Path(path).rglob("*.xml"))
     print(f"Found {len(files)} files\n")
     print(f"Loading files from {files[0]} to {files[len(files) - 1]}")
-
     events = file_loader.load_files_from_xml(files[0:500])
 
-    print("\nFilter events")
+    print(f"Filtering events by year {year} and keywords {keywords}")
     events_filtered = process_event.filter_events(events, year=year, keywords=keywords)
 
-    print(f"Parsing {len(events_filtered)} events")
-    docs = process_event.extract_passages_from_events(
-        events_filtered,
+    print(f"Creating corpus from {len(events_filtered)} events")
+
+    corpus_df = process_event.events_to_corpus(
+        events=events_filtered,
         keywords=keywords,
-        filter_words=filter_words,
+        modifier_words=modifier_words,
         context_window_sentence=context_window_sentence,
         subsequent_paragraphs=subsequent_paragraphs,
+        join_adjacent_sentences=join_adjacent_sentences,
         extract_answers=extract_answers,
+        nlp_model=nlp_model,
+        remove_additional_words=remove_additional_words,
     )
 
-    topics, probs = topic_extractor.bert_inspired(docs)
+    topic_model, topics, probs = topic_extractor.bert_inspired(
+        corpus_df["processed_text"].tolist()
+    )
 
     return topics, probs
 
