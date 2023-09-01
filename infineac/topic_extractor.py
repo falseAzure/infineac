6f8491ff5de67df86503b65c3db6e5d77d64ccd9
falseAzure <falseazure@proton.me>
2023-09-01 15:17:18 +0200

    Update topic_extractor.py
    play around

--- infineac/topic_extractor.py
+++ infineac/topic_extractor.py
@@ -3,7 +3,6 @@ This module contains helper functions for the infineac package.
 """
 
 from bertopic import BERTopic
-from bertopic.representation import KeyBERTInspired
 
 # seed_topic_list = [
 #     ["drug", "cancer", "drugs", "doctor"],
@@ -22,14 +21,31 @@ def bert_basic(docs):
     return topics, probs
 
 
-def bert_inspired(docs, nr_topics=None, seed_topic_list=None):
+def bert_inspired(docs, representation_model, nr_topics=None, seed_topic_list=None):
     # Fine-tune your topic representations
-    representation_model = KeyBERTInspired()
     topic_model = BERTopic(
         representation_model=representation_model,
         nr_topics=nr_topics,
         calculate_probabilities=True,
         seed_topic_list=seed_topic_list,
+        low_memory=True,
+        verbose=True,
+    )
+    topics, probs = topic_model.fit_transform(docs)
+    return topic_model, topics, probs
+
+
+def bert_advanced(
+    docs, representation_model, embedding_model=None, vectorizer_model=None
+):
+    # Fine-tune your topic representations
+    topic_model = BERTopic(
+        representation_model=representation_model,
+        nr_topics=10,
+        calculate_probabilities=True,
+        embedding_model=embedding_model,
+        vectorizer_model=vectorizer_model,
+        # seed_topic_list=seed_topic_list,
         low_memory=True,
         verbose=True,
     )
