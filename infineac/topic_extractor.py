e96e74cb56b8b6a310719f923bd6426d5141522c
falseAzure <falseazure@proton.me>
2023-09-12 17:10:32 +0200

    Update topic_extractor.py
    - add documentation and remove some methods

--- infineac/topic_extractor.py
+++ infineac/topic_extractor.py
@@ -1,5 +1,26 @@
 """
-This module contains helper functions for the infineac package.
+This module extracts topics from a list of documents using BERTopic.
+
+Examples
+--------
+>>> import infineac.process_event as process_event
+>>> import infineac.file_loader as file_loader
+>>> import spacy_stanza
+>>> nlp = spacy_stanza.load_pipeline("en", processors="tokenize, lemma")
+>>> nlp.add_pipe('sentencizer')
+>>> PATH_DIR = "data/transcripts/"
+>>> files = list(Path(PATH_DIR).rglob("*.xml"))
+>>> events = file_loader.load_files_from_xml(files)
+>>> keywords = {"russia": 1, "ukraine": 1}
+>>> corpus = process_event.events_to_corpus(
+                events=events,
+                keywords=keywords,
+                nlp_model=nlp)
+>>> docs = process_text.remove_sentences_under_threshold(
+                corpus["processed_text"].tolist())
+>>> topics, probs = topic_extractor.bert_advanced(docs)
+
+
 """
 
 from bertopic import BERTopic
@@ -11,28 +32,6 @@ from bertopic import BERTopic
 # ]
 
 # topic_model = BERTopic(seed_topic_list=seed_topic_list)
-
-
-def bert_basic(docs):
-    topic_model = BERTopic(
-        verbose=True, low_memory=True, nr_topics=10, calculate_probabilities=True
-    )
-    topics, probs = topic_model.fit_transform(docs)
-    return topics, probs
-
-
-def bert_inspired(docs, representation_model, nr_topics=None, seed_topic_list=None):
-    # Fine-tune your topic representations
-    topic_model = BERTopic(
-        representation_model=representation_model,
-        nr_topics=nr_topics,
-        calculate_probabilities=True,
-        seed_topic_list=seed_topic_list,
-        low_memory=True,
-        verbose=True,
-    )
-    topics, probs = topic_model.fit_transform(docs)
-    return topic_model, topics, probs
 
 
 def bert_advanced(
