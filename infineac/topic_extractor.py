5dd28cc71fdc583bf89c05df09a178dbd595478f
falseAzure <falseazure@proton.me>
2023-09-14 18:21:42 +0200

    Update topic_extractor.py
    New function to obtain groups from hierarchies

--- infineac/topic_extractor.py
+++ infineac/topic_extractor.py
@@ -25,22 +25,23 @@ Examples
 
 from bertopic import BERTopic
 
-# seed_topic_list = [
-#     ["drug", "cancer", "drugs", "doctor"],
-#     ["windows", "drive", "dos", "file"],
-#     ["space", "launch", "orbit", "lunar"],
-# ]
-
-# topic_model = BERTopic(seed_topic_list=seed_topic_list)
+import infineac.constants as constants
 
 
 def bert_advanced(
-    docs,
+    docs: list[str],
     representation_model,
     embedding_model=None,
     vectorizer_model=None,
     nr_topics=None,
+    predefined_topics: bool | list[list[str]] = None,
 ):
+    seed_topic_list = None
+    if predefined_topics is True:
+        seed_topic_list = constants.TOPICS
+    elif type(predefined_topics) == list:
+        seed_topic_list = predefined_topics
+
     # Fine-tune your topic representations
     topic_model = BERTopic(
         representation_model=representation_model,
@@ -48,9 +49,33 @@ def bert_advanced(
         embedding_model=embedding_model,
         vectorizer_model=vectorizer_model,
         nr_topics=nr_topics,
-        # seed_topic_list=seed_topic_list,
+        seed_topic_list=seed_topic_list,
         low_memory=True,
         verbose=True,
     )
+
     topics, probs = topic_model.fit_transform(docs)
     return topic_model, topics, probs
+
+
+def get_groups_from_hierarchy(hierarchical_topics, n=10):
+    """Returns the top n groups from the hierarchical topics."""
+    parent = hierarchical_topics[
+        hierarchical_topics["Distance"] == hierarchical_topics["Distance"].max()
+    ]
+    parent_id = str(parent["Parent_ID"].values[0])
+    children = {parent_id}
+    for i in range(n - 1):
+        children.remove(parent_id)
+        new_children = (
+            parent[["Child_Left_ID", "Child_Right_ID"]].values.flatten().tolist()
+        )
+        children.update(new_children)
+
+        new_df = hierarchical_topics[hierarchical_topics["Parent_ID"].isin(children)]
+        parent = new_df[new_df["Distance"] == new_df["Distance"].max()]
+        parent_id = str(parent["Parent_ID"].values[0])
+    topics_grouped = hierarchical_topics[
+        hierarchical_topics["Parent_ID"].isin(children)
+    ]
+    return topics_grouped
