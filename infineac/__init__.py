6b3924cdc57c28b046f16f28bc1b9d0b0bd058d4
falseAzure <falseazure@proton.me>
2023-09-20 10:54:00 +0200

    Update __init__.py
    add documentation

--- infineac/__init__.py
+++ infineac/__init__.py
@@ -1,3 +1,11 @@
+"""
+Extracting Financial Insights from Earnings Calls using NLP.
+InFineac is a Python package that extracts financial insights from earnings
+calls using NLP. Earnings calls are a rich source of information for investors,
+that are held quarterly by publicly traded companies. It uses the spaCy and
+BERTopic libraries for NLP.
+"""
+
 __all__ = [
     "constants",
     "file_loader",
@@ -7,3 +15,6 @@ __all__ = [
     "process_text",
     "topic_extractor",
 ]
+
+if __name__ == "__main__":
+    print("This is a package, not a script.")
