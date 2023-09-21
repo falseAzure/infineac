e291d7da349ee7f22ffd4d8b44d66084c8fa7148
falseAzure <falseazure@proton.me>
2023-09-21 16:53:21 +0200

    Update topic_extractor.py
    - new plot function
    - documentation

--- infineac/topic_extractor.py
+++ infineac/topic_extractor.py
@@ -23,8 +23,12 @@ Examples
 
 """
 
+import matplotlib.pyplot as plt
+import pandas as pd
 import polars as pl
+import seaborn as sns
 from bertopic import BERTopic
+from numpy import ndarray
 from umap import UMAP
 
 import infineac.constants as constants
@@ -38,7 +42,7 @@ def bert_advanced(
     vectorizer_model: any = None,
     nr_topics: any = None,
     predefined_topics: bool | list[list[str]] = None,
-):
+) -> tuple[BERTopic, list[int], ndarray]:
     """Extracts topics from a list of documents using BERTopic.
 
     Parameters
@@ -61,8 +65,8 @@ def bert_advanced(
 
     Returns
     -------
-    _type_
-        _description_
+    tuple[BERTopic, list[int], ndarray]
+        Tuple containing the BERTopic model, the topics and the probabilities.
     """
     seed_topic_list = None
     if predefined_topics is True:
@@ -121,8 +125,21 @@ def get_groups_from_hierarchy(
     return topics_grouped
 
 
-def get_topics_per_company(df: pl.DataFrame):
-    """Returns the topics per company."""
+def get_topics_per_company(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Returns the topics and categories per company.
+
+    Parameters
+    ----------
+    df : pl.DataFrame
+        DataFrame containing the topics and the company names as well as the
+        year and the three strategies.
+
+    Returns
+    -------
+    pl.DataFrame
+        DataFrame containing the topics, categories and strategies per company.
+    """
     df_comp = (
         df.groupby("company_name", "year")
         .agg(
@@ -150,13 +167,31 @@ def get_topics_per_company(df: pl.DataFrame):
 
 
 def categorize_topics(keywords_topics: list[list[str]]) -> pl.DataFrame:
-    """Categorizes the topics according to the corresponding `keywords_topics` and a
-    dictionary, that maps keywords to topics. Categories are grouped topics
-    defined by :func:`infineac.constants.TOPICS`."""
+    """
+    Categorizes a lists of keywords (`keywords_topics`) according to the
+    :func:`infineac.constants.TOPICS` dictionary, that maps keywords to
+    categories.
+
+    Notes
+    -----
+    Categories are grouped topics. Each list in `keywords_topics` corresponds
+    to a topic, that is a list of keywords. Each list in `keywords_topics` is
+    mapped to a category, that is a list of as well keywords.
+
+    Parameters
+    ----------
+    keywords_topics : list[list[str]]
+        List of lists of keywords per topic.
+
+    Returns
+    -------
+    pl.DataFrame
+        DataFrame containing the categories and keywords per topic.
+    """
     max_category_list = []
     for keywords in keywords_topics:
         count_max_group = 0
-        max_topic = "other"
+        max_topic = "misc"
         for category, keywords_group in constants.TOPICS.items():
             common_keywords = set(keywords_group) & set(keywords)
             count = len(common_keywords)
@@ -175,7 +210,23 @@ def categorize_topics(keywords_topics: list[list[str]]) -> pl.DataFrame:
 
 
 def map_topics_to_categories(topics: list[int], mapping: pl.DataFrame) -> list[str]:
-    """Maps the `topics` to the corresponding groups."""
+    """
+    Maps a list of `topics` to the corresponding categories.
+
+    The -1 and -2 topics are mapped to the "standard" and "empty" categories.
+
+    Parameters
+    ----------
+    topics : list[int]
+        List of topics.
+    mapping : pl.DataFrame
+        DataFrame containing the mapping from topics to categories.
+
+    Returns
+    -------
+    list[str]
+        List of categories.
+    """
     topics_pl = pl.DataFrame({"topic": topics})
     topics_pl = topics_pl.join(mapping, left_on="topic", right_on="n", how="left")
     topics_pl = topics_pl.with_columns(
@@ -189,3 +240,89 @@ def map_topics_to_categories(topics: list[int], mapping: pl.DataFrame) -> list[s
         .otherwise(pl.col("category"))
     )
     return topics_pl
+
+
+def plot_category_distribution(
+    df: pl.DataFrame, aggregate: list[str] = []
+) -> pl.DataFrame:
+    """
+    Plots the category distribution for the given DataFrame `df` and the given
+    `aggregate`.
+
+    Parameters
+    ----------
+    df : pl.DataFrame
+        DataFrame containing the categories and the strategies.
+    aggregate : list[str], default: []
+        List of columns to aggregate by.
+
+    Returns
+    -------
+    pl.DataFrame
+        DataFrame containing the category distribution.
+    """
+    count_categories = (
+        df.groupby(["category"] + aggregate)
+        .agg(
+            count_category=pl.col("category").count(),
+            exit=pl.col("exit_strategy").filter(pl.col("exit_strategy") > 0).count(),
+            adaptation=pl.col("adaptation_strategy")
+            .filter(pl.col("adaptation_strategy") > 0)
+            .count(),
+            stay=pl.col("stay_strategy").filter(pl.col("stay_strategy") > 0).count(),
+        )
+        .sort("count_category", descending=True)
+    )
+    if len(aggregate) > 0:
+        count_categories = (
+            count_categories.groupby(["category"])
+            .agg(
+                count_category=pl.col("category").count(),
+                exit=pl.col("exit").filter(pl.col("exit") > 0).count(),
+                adaptation=pl.col("adaptation")
+                .filter(pl.col("adaptation") > 0)
+                .count(),
+                stay=pl.col("stay").filter(pl.col("stay") > 0).count(),
+            )
+            .sort("count_category", descending=True)
+        )
+
+    count_categories = count_categories.with_columns(
+        (
+            pl.col("count_category")
+            - pl.col("exit")
+            - pl.col("adaptation")
+            - pl.col("stay")
+        ).alias("no strategy")
+    )
+    melted_result = pd.melt(
+        count_categories.to_pandas(),
+        id_vars="category",
+        value_vars=["no strategy", "exit", "adaptation", "stay"],
+        var_name="strategy",
+    )
+    plot_data = melted_result[
+        ~melted_result["category"].isin(["standard", "empty", "misc"])
+    ]
+
+    if len(aggregate) == 0:
+        title_add = "all mentions."
+    else:
+        title_add = " and ".join(aggregate)
+
+    ax = sns.histplot(
+        plot_data,
+        x="category",
+        hue="strategy",
+        weights="value",
+        common_norm=True,
+        multiple="stack",
+        palette="muted",
+    )
+    plt.xlabel("Category")
+    plt.ylabel("Count")
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment="right")
+    plt.title("Category and strategy distribution for " + title_add)
+    plt.show()
+
+    return count_categories
